07b31c5895e9e44999f033e2cf2d7ce4a170c3a5
zy410419243 <zy410419243@gmail.com>
2019-01-22 16:14:48 +0800

    chore: add options for index.html

---
+++ scripts/build.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const cwd = process.cwd();
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(cwd + '/dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      hash: true,
+      filename: '../index.html',
+      minify: {
+        minifyJS: true,
+        minifyCSS: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+  ],
+};
