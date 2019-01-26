230715a255326d62b4d81546d61717c598550c5a
zy410419243 <zy410419243@gmail.com>
2019-01-26 09:53:12 +0800

    chore: allow console.error and console.warn

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'linebreak-style': ['error', 'windows'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
     'no-debugger': 2,
     'no-undef': 0,
     'no-unused-vars': 0,
