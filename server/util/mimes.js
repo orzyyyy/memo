b56e6f5ba67e36979ce6f799da6101fd95b7cfba
zy410419243 <zy410419243@gmail.com>
2019-01-26 11:22:42 +0800

    chore: add favicon.ico

--- server/util/mimes.js
+++ server/util/mimes.js
@@ -1,43 +1,23 @@
-const path = require('path');
-const fs = require('fs');
-// 封装读取目录内容方法
-const dir = require('./dir');
-// 封装读取文件内容方法
-const file = require('./file');
+let mimes = {
+  css: 'text/css',
+  less: 'text/css',
+  gif: 'image/gif',
+  html: 'text/html',
+  ico: 'image/x-icon',
+  jpeg: 'image/jpeg',
+  jpg: 'image/jpeg',
+  js: 'text/javascript',
+  json: 'application/json',
+  pdf: 'application/pdf',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  swf: 'application/x-shockwave-flash',
+  tiff: 'image/tiff',
+  txt: 'text/plain',
+  wav: 'audio/x-wav',
+  wma: 'audio/x-ms-wma',
+  wmv: 'video/x-ms-wmv',
+  xml: 'text/xml',
+};
 
-/**
- * 获取静态资源内容
- * @param  {object} ctx koa上下文
- * @param  {string} 静态资源目录在本地的绝对路径
- * @return  {string} 请求获取到的本地内容
- */
-async function content(ctx, fullStaticPath) {
-  // 封装请求资源的完绝对径
-  let reqPath = path.join(fullStaticPath, ctx.url);
-
-  // 判断请求路径是否为存在目录或者文件
-  let exist = fs.existsSync(reqPath);
-
-  // 返回请求内容， 默认为空
-  let content = '';
-
-  if (!exist) {
-    //如果请求路径不存在，返回404
-    content = '404 Not Found! o(╯□╰)o！';
-  } else {
-    //判断访问地址是文件夹还是文件
-    let stat = fs.statSync(reqPath);
-
-    if (stat.isDirectory()) {
-      //如果为目录，则渲读取目录内容
-      content = dir(ctx.url, reqPath);
-    } else {
-      // 如果请求为文件，则读取文件内容
-      content = await file(reqPath);
-    }
-  }
-
-  return content;
-}
-
-module.exports = content;
+module.exports = mimes;
