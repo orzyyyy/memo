0012376855a21a5ac4a99492d0a1bf4df23f31af
orzorzorzorz <zy410419243@gmail.com>
2019-01-26 11:28:34 +0800

    feat: interaction with server (#2)
    * feat: add fetch
    
    * chore: allow console.error and console.warn
    
    * pref: press ctrl + s to interaction with server
    
    * ci: add test
    
    * chore: add favicon.ico
    
    * chore: update favicon.ico

--- server/controller/get.js
+++ server/controller/get.js
@@ -1,50 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const content = require('../util/content');
-const mimes = require('../util/mimes');
 
 const get = async ctx => {
-  // let name = ctx.params.name;
-
   ctx.type = 'html';
   ctx.response.body = fs.createReadStream(
     path.join(process.cwd(), 'index.html'),
   );
 };
 
-// 解析资源类型
-const parseMime = url => {
-  let extName = path.extname(url);
-  extName = extName ? extName.slice(1) : 'unknown';
-  return mimes[extName];
-};
-
-const getDist = async ctx => {
-  // 静态资源目录在本地的绝对路径
-  const fullStaticPath = path.join(process.cwd(), '/dist');
-  // 获取静态资源内容，有可能是文件内容，目录，或404
-  const contentIns = await content(ctx, fullStaticPath);
-  // 解析请求内容的类型
-  const mime = parseMime(ctx.url);
-
-  // 如果有对应的文件类型，就配置上下文的类型
-  if (mime) {
-    ctx.type = mime;
-  }
-
-  // 输出静态资源内容
-  if (mime && mime.indexOf('image/') >= 0) {
-    // 如果是图片，则用node原生res，输出二进制数据
-    ctx.res.writeHead(200);
-    ctx.res.write(contentIns, 'binary');
-    ctx.res.end();
-  } else {
-    // 其他则输出文本
-    ctx.body = contentIns;
-  }
-};
-
 module.exports = {
   'GET /': get,
-  // 'GET /dist': getDist,
 };
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ const path = require('path');
 const content = require('./util/content');
 const mimes = require('./util/mimes');
 
-// 解析资源类型
 const parseMime = url => {
   let extName = path.extname(url);
   extName = extName ? extName.slice(1) : 'unknown';
@@ -19,16 +18,11 @@ const addMapping = (router, mapping) => {
     let path = url.substring(4);
     if (url.startsWith('GET ')) {
       router.get(path, mapping[url]);
-      // eslint-disable-next-line
-      console.log(`register URL mapping: GET ${path}`);
     } else if (url.startsWith('POST ')) {
       path = url.substring(5);
       router.post(path, mapping[url]);
-      // eslint-disable-next-line
-      console.log(`register URL mapping: POST ${path}`);
     } else {
-      // eslint-disable-next-line
-      console.log(`invalid URL: ${url}`);
+      console.error(`invalid URL: ${url}`);
     }
   }
 };
@@ -50,26 +44,19 @@ app.use(bodyParser());
 app.use(router.routes());
 
 app.use(async ctx => {
-  // 静态资源目录在本地的绝对路径
   const fullStaticPath = path.join(process.cwd());
-  // 获取静态资源内容，有可能是文件内容，目录，或404
   const contentIns = await content(ctx, fullStaticPath);
-  // 解析请求内容的类型
   const mime = parseMime(ctx.url);
 
-  // 如果有对应的文件类型，就配置上下文的类型
   if (mime) {
     ctx.type = mime;
   }
 
-  // 输出静态资源内容
   if (mime && mime.indexOf('image/') >= 0) {
-    // 如果是图片，则用node原生res，输出二进制数据
     ctx.res.writeHead(200);
     ctx.res.write(contentIns, 'binary');
     ctx.res.end();
   } else {
-    // 其他则输出文本
     ctx.body = contentIns;
   }
 });
