16b36591e9e102fa2d90afbb705c90640900595f
orzorzorzorz <zy410419243@gmail.com>
2019-01-28 09:11:45 +0800

    refactor: 插件处理静态文件 & 尝试按需加载 (#6)
    * refactor: 引入 koa-static 加载静态文件 & 初始化详情页
    
    * fix: 无法读取 chunk
    
    * style: 修改代码格式

--- server/index.js
+++ server/index.js
@@ -3,15 +3,8 @@ const app = new Koa();
 const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser');
 const fs = require('fs');
+const KoaStatic = require('koa-static');
 const path = require('path');
-const content = require('./util/content');
-const mimes = require('./util/mimes');
-
-const parseMime = url => {
-  let extName = path.extname(url);
-  extName = extName ? extName.slice(1) : 'unknown';
-  return mimes[extName];
-};
 
 const addMapping = (router, mapping) => {
   for (let url in mapping) {
@@ -43,23 +36,8 @@ addControllers(router);
 app.use(bodyParser());
 app.use(router.routes());
 
-app.use(async ctx => {
-  const fullStaticPath = path.join(process.cwd());
-  const contentIns = await content(ctx, fullStaticPath);
-  const mime = parseMime(ctx.url);
-
-  if (mime) {
-    ctx.type = mime;
-  }
-
-  if (mime && mime.indexOf('image/') >= 0) {
-    ctx.res.writeHead(200);
-    ctx.res.write(contentIns, 'binary');
-    ctx.res.end();
-  } else {
-    ctx.body = contentIns;
-  }
-});
+app.use(KoaStatic(process.cwd()));
+app.use(KoaStatic(path.join(process.cwd(), 'dist')));
 
 app.listen(9099);
 // eslint-disable-next-line
---
+++ src/pages/mapping.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import '../assets/mapping.css';
+
+export default class mapping extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount = () => {
+    // eslint-disable-next-line
+    console.log(this.props.id);
+  };
+
+  render = () => {
+    return <div className="mapping">mapping</div>;
+  };
+}
