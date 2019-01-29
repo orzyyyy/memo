3bf5fa1a16f8f52418865b0b14554cd0ee139c05
orzorzorzorz <zy410419243@gmail.com>
2019-01-29 19:39:52 +0800

    feat: 查看及修改 mapping (#8)
    * feat: 加载 mapping & 保存 mapping
    
    * feat: 新增保存时写入文件
    
    * pref: mapping 修改

--- server/controller/save.js
+++ server/controller/save.js
@@ -1,6 +1,20 @@
-const saveLayout = async (ctx, next) => {
+const fs = require('fs');
+const md5 = require('blueimp-md5');
+const path = require('path');
+
+const saveLayout = async ctx => {
   const body = ctx.request.body;
-  ctx.response.body = body.layout;
+  const layout = body.layout;
+  const id = body.id || md5(new Date().getTime());
+  const writeFiles = [`src/layout/${id}.json`, `dist/layout/${id}.json`];
+  try {
+    for (let item of writeFiles) {
+      fs.writeFileSync(path.join(process.cwd(), item), JSON.stringify(layout));
+    }
+    ctx.response.body = true;
+  } catch (error) {
+    ctx.response.body = false;
+  }
 };
 
 module.exports = {
--- src/pages/mapping.js
+++ src/pages/mapping.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
 import '../assets/mapping.css';
+import { ajax } from '../urlHelper';
+import Canvas from 'mini-xmind/dist/lib/canvas';
+import Toolbar from 'mini-xmind/dist/lib/toolbar';
 
 export default class mapping extends Component {
   static propTypes = {};
@@ -10,15 +12,68 @@ export default class mapping extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      data: {},
+    };
   }
 
   componentDidMount = () => {
-    // eslint-disable-next-line
-    console.log(location);
+    this.bindKeyDown();
+    this.getMapping();
+  };
+
+  bindKeyDown = () => {
+    document.onkeydown = e => {
+      const { ctrlKey, keyCode } = e;
+
+      // ctrl + s
+      if (ctrlKey && keyCode === 83) {
+        const data = DataCollector.getAll();
+        this.send(data);
+      }
+      return false;
+    };
+  };
+
+  send = data => {
+    const id = this.getMappingId();
+    ajax({
+      url: 'save',
+      params: {
+        method: 'POST',
+        body: JSON.stringify({ layout: data, id }),
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+      },
+      success: result => {
+        if (!result) {
+          console.error('error with save');
+        }
+      },
+    });
+  };
+
+  getMapping = () => {
+    const id = this.getMappingId();
+
+    ajax({
+      url: `dist/layout/${id}.json`,
+      success: data => this.setState({ data }),
+    });
+  };
+
+  getMappingId = () => {
+    const hash = location.hash.split('/');
+    return hash[hash.length - 1];
   };
 
   render = () => {
-    return <div className="mapping">mapping</div>;
+    const { data } = this.state;
+    return (
+      <div className="mapping">
+        <Toolbar />
+        <Canvas data={data} className="canvas" />
+      </div>
+    );
   };
 }
