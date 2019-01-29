284dd70bfb1f09889ce01fca2d579d5748d26868
zy410419243 <zy410419243@gmail.com>
2019-01-29 17:41:11 +0800

    feat: 加载 mapping & 保存 mapping

--- src/pages/mapping.js
+++ src/pages/mapping.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-
 import '../assets/mapping.css';
+import { ajax } from '../urlHelper';
+import Canvas from 'mini-xmind/dist/lib/canvas';
+import Toolbar from 'mini-xmind/dist/lib/toolbar';
 
 export default class mapping extends Component {
   static propTypes = {};
@@ -10,15 +12,66 @@ export default class mapping extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      data: {},
+    };
   }
 
   componentDidMount = () => {
-    // eslint-disable-next-line
-    console.log(location);
+    this.bindKeyDown();
+    this.getMapping();
+  };
+
+  bindKeyDown = () => {
+    document.onkeydown = e => {
+      const { ctrlKey, keyCode } = e;
+
+      // ctrl + s
+      if (ctrlKey && keyCode === 83) {
+        const data = DataCollector.getAll();
+        this.send(data);
+      }
+      return false;
+    };
+  };
+
+  send = data => {
+    ajax({
+      url: 'save',
+      params: {
+        method: 'POST',
+        body: JSON.stringify({ layout: data }),
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+      },
+      success: result => {
+        // eslint-disable-next-line
+        console.log(result);
+      },
+    });
+  };
+
+  getMapping = () => {
+    const id = this.getMappingId();
+
+    ajax({
+      url: `dist/layout/${id}.json`,
+      success: data => this.setState({ data }),
+    });
+  };
+
+  getMappingId = () => {
+    const hash = location.hash.split('/');
+    return hash[hash.length - 1];
   };
 
   render = () => {
-    return <div className="mapping">mapping</div>;
+    const { data } = this.state;
+    return (
+      <div className="mapping">
+        <Toolbar />
+        <Canvas data={data} className="canvas" />
+      </div>
+    );
   };
 }
