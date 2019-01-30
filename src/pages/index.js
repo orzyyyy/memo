ac183813f7d374b3646fa8fca8221da3a5e4c1ff
zy410419243 <zy410419243@gmail.com>
2019-01-30 11:39:06 +0800

    web: 增加删除事件

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,23 +30,35 @@ export default class MainPage extends Component {
     location.hash = `/${id}`;
   };
 
-  handleDelete = ({ item, key, keyPath }) => {
-    // eslint-disable-next-line
-    console.log(item, key, keyPath);
+  handleDelete = ({ id }) => {
+    ajax({
+      url: 'del',
+      params: {
+        method: 'DELETE',
+        body: JSON.stringify({ id }),
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+      },
+      success: result => {
+        if (!result) {
+          console.error('error with delete');
+        }
+      },
+    });
   };
 
   generateMainPage = () => {
     const { data } = this.state;
-    const menu = (
-      <Menu>
-        <Menu.Item key="1">修改</Menu.Item>
-        <Menu.Item key="2" onClick={this.handleDelete}>
-          删除
-        </Menu.Item>
-      </Menu>
-    );
     return data.map(item => {
       const { thumbnailUrl, id, hoverText } = item;
+      const menu = (
+        <Menu>
+          <Menu.Item key="update">修改</Menu.Item>
+          <Menu.Item key="delete" onClick={() => this.handleDelete(item)}>
+            删除
+          </Menu.Item>
+        </Menu>
+      );
       return (
         <Card.Grid className="card" key={id}>
           <Dropdown overlay={menu} trigger={['contextMenu']}>
