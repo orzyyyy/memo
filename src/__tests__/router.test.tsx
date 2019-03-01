07d8949793d0aa4e13462a8b5cea4e613d8be073
zy410419243 <zy410419243@gmail.com>
2019-03-01 12:53:34 +0800

    test: update case for router

--- src/__tests__/router.test.tsx
+++ src/__tests__/router.test.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Router from '../router';
+import mapping from '../mapping.json';
 import 'nino-cli/scripts/setup';
 
 describe('Router', () => {
   it('render correctly', () => {
-    expect(shallow(<Router />)).toMatchSnapshot();
+    const wrapper = shallow(<Router />);
+    expect(wrapper.find('Route').length).toBe(mapping.length + 2);
   });
 });
