3f63b0d1825aaefc6b39682d2b26354a3911efc8
zy410419243 <zy410419243@gmail.com>
2019-05-25 08:15:53 +0800

    refactor(server): generate all static files by server

--- server/controller/save.ts
+++ server/controller/save.ts
@@ -1,15 +1,40 @@
-const fs = require('fs-extra');
-const md5 = require('blueimp-md5');
-const path = require('path');
-const generate = require('../generate');
+import fs from 'fs-extra';
+import md5 from 'blueimp-md5';
+import path from 'path';
 
-const saveLayout = async (ctx: any) => {
+export interface MappingProps {
+  createTime?: string;
+  modifyTime?: string;
+  id?: string;
+  title?: string;
+  url?: string;
+}
+
+const updateMappingRouter = (targetItem: MappingProps) => {
+  const writeFilesPaths = [
+    `src/assets/mapping.json`,
+    `dist/assets/mapping.json`,
+  ];
+  for (const item of writeFilesPaths) {
+    fs.outputJSON(path.join(process.cwd(), item), targetItem, {
+      spaces: 2,
+    }).catch((err: any) => {
+      console.error(err);
+    });
+  }
+  // tslint:disable-next-line: no-console
+  console.log('update mapping router completed');
+};
+
+const updateMapping = async (ctx: any) => {
   const body = ctx.request.body;
-  const layout = body.layout;
-  const id = body.id || md5(new Date().getTime());
-  const writeFiles = [`src/layout/${id}.json`, `dist/layout/${id}.json`];
+  const { layout, id, title, url } = body.layout;
+  const writeFilesPaths = [
+    `src/assets/mapping/${id}.json`,
+    `dist/assets/mapping/${id}.json`,
+  ];
   try {
-    for (const item of writeFiles) {
+    for (const item of writeFilesPaths) {
       fs.outputJSON(path.join(process.cwd(), item), layout, {
         spaces: 2,
       })
@@ -21,29 +46,56 @@ const saveLayout = async (ctx: any) => {
           console.error(err);
         });
     }
+    const targetFile = fs.readFileSync(
+      path.join(process.cwd(), `src/assets/mapping/${id}.json`),
+    );
+    const { createTime, title: originTitle, url: originUrl } = JSON.parse(
+      targetFile.toString(),
+    );
+    updateMappingRouter({
+      createTime,
+      modifyTime: new Date().getTime().toString(),
+      id,
+      title: title || originTitle,
+      url: url || originUrl,
+    });
     ctx.response.body = true;
   } catch (error) {
-    ctx.response.body = false;
+    ctx.response.body = error.message;
   }
 };
 
-const initMapping = async (ctx: any) => {
-  const id = md5(new Date().getTime());
-  const writeFiles = [`src/layout/${id}.json`, `dist/layout/${id}.json`];
+// 1. generate empty file in assets/mapping for mapping info
+// 2. update router file
+const initNewMapping = async (ctx: any) => {
+  const { url, title } = ctx.request.body;
+  const dateTime = new Date().getTime().toString();
+  const id = md5(dateTime);
+  const writeFilesPaths = [
+    `src/assets/mapping/${id}.json`,
+    `dist/assets/mapping/${id}.json`,
+  ];
   try {
-    for (const item of writeFiles) {
+    // 1. generate empty file in assets/mapping for mapping info
+    for (const item of writeFilesPaths) {
       fs.writeFileSync(path.join(process.cwd(), item), '');
     }
+    // 2. update router file
+    updateMappingRouter({
+      createTime: dateTime,
+      modifyTime: dateTime,
+      id,
+      title,
+      url,
+    });
     ctx.response.body = id;
-    generate();
   } catch (error) {
     console.error(error);
-    ctx.response.body = false;
+    ctx.response.body = error.message;
   }
 };
 
 module.exports = {
-  'POST /save': saveLayout,
-  'POST /save/new': initMapping,
+  'POST /save/mapping/update': updateMapping,
+  'POST /save/mapping/new': initNewMapping,
 };
-export {};
