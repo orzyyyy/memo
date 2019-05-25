15736bf4c1ec173969f260490aaa1721fec83155
zy410419243 <zy410419243@gmail.com>
2019-05-25 18:17:50 +0800

    feat: generate sider of menu

---
+++ scripts/generateSider.js
@@ -0,0 +1,41 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const mappingPath = path.join(process.cwd(), 'src/assets/mapping.json');
+const mappingFile = fs.readFileSync(mappingPath);
+const result = JSON.parse(mappingFile.toString());
+
+let types = [];
+result.map(item => {
+  types.push(item.type);
+});
+types = Array.from(new Set(types));
+
+let menu = [];
+for (const type of types) {
+  menu.push({
+    key: type,
+    title: type,
+    children: [],
+  });
+}
+for (const item of result) {
+  for (const menuItem of menu) {
+    if (menuItem.key === item.type) {
+      menuItem.children.push({
+        key: item.subType,
+        value: item.subType,
+      });
+    }
+  }
+}
+
+fs.outputJSON(path.join(process.cwd(), 'src/assets/sider.json'), menu, {
+  spaces: 2,
+})
+  .then(() => {
+    console.log('generate menu-sider completed.');
+  })
+  .catch(err => {
+    console.error(err);
+  });
