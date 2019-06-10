7e1b30873bb608882df89d044858880c2b6b3650
zy410419243 <zy410419243@gmail.com>
2019-06-10 11:04:23 +0800

    test(MarkdownEditor): update cases

--- src/pages/__tests__/MarkdownEditor.test.tsx
+++ src/pages/__tests__/MarkdownEditor.test.tsx
@@ -14,4 +14,25 @@ describe('MarkdownEditor', () => {
     );
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('onSave, onChange works', () => {
+    const onSave = jest.fn();
+    const wrapper = mount(
+      <MarkdownEditor
+        targetId="targetId"
+        dataSource={'testDataSource'}
+        onSave={onSave}
+      />,
+    );
+    wrapper
+      .find('Button')
+      .props()
+      .onClick();
+    expect(onSave).toHaveBeenCalled();
+    wrapper
+      .find('TextArea')
+      .props()
+      .onChange({ target: { value: 'test' } });
+    expect(wrapper.state().textareaValue).toBe('test');
+  });
 });
