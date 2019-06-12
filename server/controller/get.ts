ec4496c44b2f4d2019bbaae43ebbc1933350f715
zy410419243 <zy410419243@gmail.com>
2019-06-12 18:36:54 +0800

    feat(server): get detail info for every block

--- server/controller/get.ts
+++ server/controller/get.ts
@@ -3,6 +3,13 @@ const path = require('path');
 const puppeteer = require('puppeteer-core');
 const exHentaiCookie = require('../resource/exHentaiCookie');
 
+export interface ExHentaiInfoItem {
+  name: string;
+  detailUrl: string;
+  postTime: number;
+  thumbnailUrl: string;
+}
+
 const get = async (ctx: any) => {
   ctx.type = 'html';
   ctx.response.body = fs.createReadStream(
@@ -16,7 +23,7 @@ const setExHentaiCookie = async (page: any) => {
   }
 };
 
-const getExhentaiImageSrc = async ({
+const getExHentaiInfo = async ({
   pageIndex,
   page,
 }: {
@@ -29,17 +36,31 @@ const getExhentaiImageSrc = async ({
       pageIndex,
     { waitUntil: 'domcontentloaded' },
   );
-  const result = await page.$$eval(
-    'img',
-    (images: any[]) =>
+  const exHentaiInfo = await page.$$eval(
+    'div.gl1t',
+    (wrappers: any[]) =>
       new Promise(resolve => {
-        const filterImages = images
-          .filter((image: any) => !image.title.includes('torrent'))
-          .map((item: any) => (item = item.src));
-        resolve(filterImages);
+        const results: ExHentaiInfoItem[] = [];
+        for (const item of wrappers) {
+          const tempPostTime = item.lastChild.innerText.replace(/[^0-9]/gi, '');
+          const year = tempPostTime.substring(0, 4);
+          const month = tempPostTime.substring(5, 6) - 1;
+          const day = tempPostTime.substring(7, 8);
+          const hour = tempPostTime.substring(9, 10);
+          const minute = tempPostTime.substring(11, 12);
+
+          const postTime = new Date(year, month, day, hour, minute).getTime();
+          results.push({
+            name: item.firstChild.innerText,
+            detailUrl: item.firstChild.href,
+            postTime,
+            thumbnailUrl: item.childNodes[1].firstChild.firstChild.src,
+          });
+        }
+        resolve(results);
       }),
   );
-  return result;
+  return exHentaiInfo;
 };
 
 const getExhentai = async (ctx: any) => {
@@ -47,15 +68,18 @@ const getExhentai = async (ctx: any) => {
     executablePath:
       'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
     args: ['--proxy-server=127.0.0.1:1080'],
+    devtools: true,
   });
   const page = await browser.newPage();
+
   setExHentaiCookie(page);
-  let results: string[] = [];
-  for (let i = 0; i < 5; i++) {
-    const result = await getExhentaiImageSrc({ pageIndex: i, page });
+  let results: ExHentaiInfoItem[] = [];
+  for (let i = 0; i < 1; i++) {
+    const result = await getExHentaiInfo({ pageIndex: i, page });
     results = [...results, ...result];
     await page.waitFor(4000);
   }
+
   await browser.close();
   ctx.response.body = JSON.stringify(results);
 };
