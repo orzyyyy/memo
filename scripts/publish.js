d56099a034bbc346f2dfaccdebe4fcea9d7a6b6a
zy410419243 <zy410419243@gmail.com>
2019-06-13 14:30:26 +0800

    chore: use node api of gh-pages

---
+++ scripts/publish.js
@@ -0,0 +1,10 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish('dist', {
+  dotfiles: true,
+  repo: 'https://github.com/zy410419243/memo.git',
+  user: {
+    name: 'zy410419243',
+    email: 'zy410419243@gmail.com',
+  },
+});
