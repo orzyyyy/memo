5d739ca463e6a0e8be3e7270063b7ca26acf5344
zy410419243 <zy410419243@gmail.com>
2019-06-13 14:55:13 +0800

    chore: catch error when publishing

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,11 +1,17 @@
 const ghpages = require('gh-pages');
 
-ghpages.publish('dist', {
-  dotfiles: true,
-  repo: 'https://github.com/zy410419243/memo.git',
-  user: {
-    name: 'zy410419243',
-    email: 'zy410419243@gmail.com',
+ghpages.publish(
+  'dist',
+  {
+    dotfiles: true,
+    repo: 'https://github.com/zy410419243/memo.git',
+    user: {
+      name: 'zy410419243',
+      email: 'zy410419243@gmail.com',
+    },
   },
-});
+  error => {
+    throw Error(error.message);
+  },
+);
 console.log('published');
