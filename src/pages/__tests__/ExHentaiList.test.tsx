2475ec7fc2a94cd805cb0704ca693d30621f4ff2
zy410419243 <zy410419243@gmail.com>
2019-06-15 08:04:22 +0800

    test: update cases for ExHentaiList

---
+++ src/pages/__tests__/ExHentaiList.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import ExHentaiList from '../ExHentaiList';
+import 'nino-cli/scripts/setup';
+
+describe('ExHentaiList', () => {
+  it('render correctly', () => {
+    const wrapper = shallow(
+      <ExHentaiList
+        dataSource={[
+          {
+            name:
+              '[Zakotsu] Amanita Pantherina (COMIC Koh 2018-06) [Chinese] [嗷呜个人汉化] [Digital]',
+            detailUrl: 'https://exhentai.org/g/1430261/17651c12eb/',
+            postTime: 1559412420000,
+            thumbnailUrl:
+              'https://exhentai.org/t/5b/47/5b47c48abf0fb6c685869520cdb8ab0d97990e4d-1074410-1280-1807-jpg_250.jpg',
+          },
+        ]}
+      />,
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
