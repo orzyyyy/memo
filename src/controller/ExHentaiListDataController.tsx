162a928e76640cb71c4caa4b89b6db0ea59b737b
zy410419243 <zy410419243@gmail.com>
2019-06-17 17:52:52 +0800

    feat: add empty status for online page

--- src/controller/ExHentaiListDataController.tsx
+++ src/controller/ExHentaiListDataController.tsx
@@ -1,36 +1,47 @@
 import React, { Component } from 'react';
 import ExHentaiList from '../pages/ExHentaiList';
 import { ExHentaiInfoItem } from '../../server/controller/get';
+import { Empty } from 'antd';
 
 export interface ExHentaiListDataControllerState {
-  dataSource: ExHentaiInfoItem[];
+  dataSource: ExHentaiInfoItem[] | null;
 }
 
 export default class ExHentaiListDataController extends Component<
   any,
   ExHentaiListDataControllerState
 > {
-  state = {
+  state: ExHentaiListDataControllerState = {
     dataSource: [],
   };
 
-  componentDidMount = async () => {
-    const lastestSet = await this.getExHentaiLastestSet();
-    const dataSource = await this.getExHentaiData(lastestSet);
-    this.setState({ dataSource });
-  };
-
-  getExHentaiLastestSet = async () => {
-    const response = await fetch('/exhentai/getLastestSet');
-    return await response.text();
-  };
-
-  getExHentaiData = async (url: string) => {
-    const response = await fetch(url);
-    return await response.json();
+  componentDidMount = () => {
+    fetch('/exhentai/getLastestSet1')
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        } else {
+          throw new Error();
+        }
+      })
+      .then((url: string) => {
+        fetch(url)
+          .then(response => response.json())
+          .then(dataSource => this.setState({ dataSource }));
+      })
+      .catch(() => {
+        this.setState({ dataSource: null });
+        console.error('fetch lastestSet error');
+      });
   };
 
   render() {
-    return <ExHentaiList dataSource={this.state.dataSource} />;
+    const notify = <Empty description={`该页面仅在本地可用`} />;
+    return (
+      <>
+        {!this.state.dataSource && notify}
+        <ExHentaiList dataSource={this.state.dataSource} />
+      </>
+    );
   }
 }
--- src/pages/ExHentaiList.tsx
+++ src/pages/ExHentaiList.tsx
@@ -4,7 +4,7 @@ import { Row, Col, Card, Dropdown, Menu } from 'antd';
 import LazyLoad from 'react-lazyload';
 
 export interface ExHentaiListProps {
-  dataSource: ExHentaiInfoItem[];
+  dataSource: ExHentaiInfoItem[] | null;
 }
 
 const menu = (
@@ -19,21 +19,22 @@ const openDetail = (url: string) => {
 
 export default ({ dataSource }: ExHentaiListProps) => (
   <Row gutter={16} style={{ width: '100%' }}>
-    {dataSource.map((item, i) => (
-      <Col span={4} key={item.name + i}>
-        <LazyLoad height={document.body.clientHeight} once>
-          <Dropdown overlay={menu} trigger={['contextMenu']}>
-            <Card
-              hoverable
-              cover={<img alt={item.name} src={item.thumbnailUrl} />}
-              style={{ height: 500 }}
-              onClick={() => openDetail(item.detailUrl)}
-            >
-              <Card.Meta title={item.name} />
-            </Card>
-          </Dropdown>
-        </LazyLoad>
-      </Col>
-    ))}
+    {dataSource &&
+      dataSource.map((item, i) => (
+        <Col span={4} key={item.name + i}>
+          <LazyLoad height={document.body.clientHeight} once>
+            <Dropdown overlay={menu} trigger={['contextMenu']}>
+              <Card
+                hoverable
+                cover={<img alt={item.name} src={item.thumbnailUrl} />}
+                style={{ height: 500 }}
+                onClick={() => openDetail(item.detailUrl)}
+              >
+                <Card.Meta title={item.name} />
+              </Card>
+            </Dropdown>
+          </LazyLoad>
+        </Col>
+      ))}
   </Row>
 );
