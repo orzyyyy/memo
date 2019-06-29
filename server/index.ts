e4ac9ecc0c74d73f02a2289ae73d3c3ddf3c3b5f
orzyyyy <zy410419243@gmail.com>
2019-06-30 07:01:52 +0800

    chore: update user.name

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import KoaStatic from 'koa-static';
 import Router from './router';
+import { info } from './utils/log';
 
 const app = new Koa();
 const { router } = Router;
@@ -11,6 +12,5 @@ app.use(router.routes());
 app.use(KoaStatic(process.cwd() + '/dist'));
 
 export default app.listen(9099, () => {
-  // tslint:disable-next-line: no-console
-  console.log('listen at port 9099');
+  info('listen at port 9099');
 });
