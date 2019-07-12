2ac9e4c038192e9e918d132c20a45d19e1cf9275
orzyyyy <zy410419243@gmail.com>
2019-07-12 08:55:12 +0800

    fix: nino can't detect changes in src (#118)
    * fix: nino can't detect changes in src
    
    * fix: build problem caused by missing tsc output

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,7 @@ const cwd = process.cwd();
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+  entry: { ninoninoni: path.join(__dirname, '../dist/src') },
   plugins: [
     new CopyWebpackPlugin([
       {
@@ -12,6 +13,10 @@ module.exports = {
       {
         from: path.join(cwd, '.circleci/config.yml'),
         to: path.join(cwd, 'dist/.circleci/config.yml'),
+      },
+      {
+        from: path.join(cwd, 'src/pages/css'),
+        to: path.join(cwd, 'dist/src/pages/css'),
       },
     ]),
   ],
