b3307e943201a74d457724683e1b66c45c2422f6
orzyyyy <zy410419243@gmail.com>
2019-08-01 18:01:11 +0800

    feat: live reload with socket.io (#143)
    * feat: live reload with socket.io
    
    * fix: can not quit when running build

--- scripts/build.js
+++ scripts/build.js
@@ -2,29 +2,42 @@ const path = require('path');
 const cwd = process.cwd();
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const PostCompile = require('post-compile-webpack-plugin');
+const IO = require('socket.io-client');
+
+const plugins = [
+  new CopyWebpackPlugin([
+    {
+      from: path.join(cwd, 'src/assets'),
+      to: path.join(cwd, 'dist/assets'),
+    },
+    {
+      from: path.join(cwd, '.circleci/config.yml'),
+      to: path.join(cwd, 'dist/.circleci/config.yml'),
+    },
+    {
+      from: path.join(cwd, 'src/pages/css'),
+      to: path.join(cwd, 'dist/src/pages/css'),
+    },
+  ]),
+  // new BundleAnalyzerPlugin(),
+];
+
+if (process.env.BUILD_ENV !== 'prod') {
+  plugins.push(
+    new PostCompile(() => {
+      const socket = IO('http://localhost:9099');
+      socket.emit('refresh');
+    }),
+  );
+}
 
 module.exports = {
   entry: {
     ninoninoni: path.join(__dirname, '../dist/src'),
-    reactBase: ['react', 'react-dom'],
+    'react-base': ['react', 'react-dom'],
   },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.join(cwd, 'src/assets'),
-        to: path.join(cwd, 'dist/assets'),
-      },
-      {
-        from: path.join(cwd, '.circleci/config.yml'),
-        to: path.join(cwd, 'dist/.circleci/config.yml'),
-      },
-      {
-        from: path.join(cwd, 'src/pages/css'),
-        to: path.join(cwd, 'dist/src/pages/css'),
-      },
-    ]),
-    // new BundleAnalyzerPlugin(),
-  ],
+  plugins,
   optimization: {
     splitChunks: {
       cacheGroups: {
