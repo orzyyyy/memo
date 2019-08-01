461b8cb89d0fdb57a1240b095360e669c6164178
orzyyyy <zy410419243@gmail.com>
2019-08-01 17:58:56 +0800

    fix: can not quit when running build

--- scripts/build.js
+++ scripts/build.js
@@ -5,32 +5,39 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const PostCompile = require('post-compile-webpack-plugin');
 const IO = require('socket.io-client');
 
+const plugins = [
+  new CopyWebpackPlugin([
+    {
+      from: path.join(cwd, 'src/assets'),
+      to: path.join(cwd, 'dist/assets'),
+    },
+    {
+      from: path.join(cwd, '.circleci/config.yml'),
+      to: path.join(cwd, 'dist/.circleci/config.yml'),
+    },
+    {
+      from: path.join(cwd, 'src/pages/css'),
+      to: path.join(cwd, 'dist/src/pages/css'),
+    },
+  ]),
+  // new BundleAnalyzerPlugin(),
+];
+
+if (process.env.BUILD_ENV !== 'prod') {
+  plugins.push(
+    new PostCompile(() => {
+      const socket = IO('http://localhost:9099');
+      socket.emit('refresh');
+    }),
+  );
+}
+
 module.exports = {
   entry: {
     ninoninoni: path.join(__dirname, '../dist/src'),
     'react-base': ['react', 'react-dom'],
   },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: path.join(cwd, 'src/assets'),
-        to: path.join(cwd, 'dist/assets'),
-      },
-      {
-        from: path.join(cwd, '.circleci/config.yml'),
-        to: path.join(cwd, 'dist/.circleci/config.yml'),
-      },
-      {
-        from: path.join(cwd, 'src/pages/css'),
-        to: path.join(cwd, 'dist/src/pages/css'),
-      },
-    ]),
-    // new BundleAnalyzerPlugin(),
-    new PostCompile(() => {
-      const socket = IO('http://localhost:9099');
-      socket.emit('refresh');
-    }),
-  ],
+  plugins,
   optimization: {
     splitChunks: {
       cacheGroups: {
