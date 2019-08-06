2012b3aa735443513c6614be744782b313589348
orzyyyy <zy410419243@gmail.com>
2019-08-07 07:49:00 +0800

    perf: check status for whether to bind socket.io

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,4 @@ import App from './router';
 
 const MOUNT_NODE = document.getElementById('root');
 
-if (process.env.BUILD_ENV !== 'prod') {
-  import('socket.io-client').then(target => {
-    const socket = target.default('http://localhost:9099');
-    socket.on('refresh', () => {
-      location.reload();
-    });
-  });
-}
-
 ReactDOM.render(<App />, MOUNT_NODE);
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
-import React, { Suspense, lazy } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { Component, Suspense, lazy } from 'react';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import TohoLoading from './pages/TohoLoading';
 const MainPage = lazy(() => import('./controller/MainPageDataController'));
 const MappingDetailDataController = lazy(() =>
@@ -9,18 +9,51 @@ const MarkdownEditorDataController = lazy(() =>
   import('./controller/MarkdownEditorDataController'),
 );
 
-export default () => (
-  <Router>
-    <Suspense fallback={<TohoLoading />}>
-      <Switch>
-        <Route path="/" component={MainPage} exact />
-        <Route path="/mapping/:id" component={MappingDetailDataController} />
-        <Route
-          path="/markdown/edit/:id"
-          component={MarkdownEditorDataController}
-        />
-        <Route path="/markdown/:id" component={MarkdownEditorDataController} />
-      </Switch>
-    </Suspense>
-  </Router>
-);
+const bindSocket = () => {
+  import('socket.io-client').then(target => {
+    const socket = target.default('http://localhost:9099');
+    socket.on('refresh', () => {
+      location.reload();
+    });
+  });
+};
+
+export default class Router extends Component {
+  componentDidMount() {
+    fetch('/test')
+      .then(response => {
+        if (response.ok) {
+          return response.text();
+        }
+        throw new Error();
+      })
+      .then(() => {
+        bindSocket();
+      })
+      .catch();
+  }
+
+  render() {
+    return (
+      <HashRouter>
+        <Suspense fallback={<TohoLoading />}>
+          <Switch>
+            <Route path="/" component={MainPage} exact />
+            <Route
+              path="/mapping/:id"
+              component={MappingDetailDataController}
+            />
+            <Route
+              path="/markdown/edit/:id"
+              component={MarkdownEditorDataController}
+            />
+            <Route
+              path="/markdown/:id"
+              component={MarkdownEditorDataController}
+            />
+          </Switch>
+        </Suspense>
+      </HashRouter>
+    );
+  }
+}
