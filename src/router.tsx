8639954deec9255316de2a3a5c20f6b2a2a53a0d
orzyyyy <zy410419243@gmail.com>
2019-08-07 15:00:36 +0800

    fix: conflicts

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 import React, { Suspense, lazy } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-=======
-import React, { Component, Suspense, lazy } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
->>>>>>> feat: replace HashRouter with BrowserRouter
 import TohoLoading from './pages/TohoLoading';
 const MainPage = lazy(() => import('./controller/MainPageDataController'));
 const MappingDetailDataController = lazy(() =>
@@ -14,9 +9,8 @@ const MarkdownEditorDataController = lazy(() =>
   import('./controller/MarkdownEditorDataController'),
 );
 
-<<<<<<< HEAD
 export default () => (
-  <HashRouter>
+  <BrowserRouter>
     <Suspense fallback={<TohoLoading />}>
       <Switch>
         <Route path="/" component={MainPage} exact />
@@ -28,55 +22,5 @@ export default () => (
         <Route path="/markdown/:id" component={MarkdownEditorDataController} />
       </Switch>
     </Suspense>
-  </HashRouter>
+  </BrowserRouter>
 );
-=======
-const bindSocket = () => {
-  import('socket.io-client').then(target => {
-    const socket = target.default('http://localhost:9099');
-    socket.on('refresh', () => {
-      location.reload();
-    });
-  });
-};
-
-export default class Router extends Component {
-  componentDidMount() {
-    fetch('/test')
-      .then(response => {
-        if (response.ok) {
-          return response.text();
-        }
-        throw new Error();
-      })
-      .then(() => {
-        bindSocket();
-      })
-      .catch();
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <Suspense fallback={<TohoLoading />}>
-          <Switch>
-            <Route path="/" component={MainPage} exact />
-            <Route
-              path="/mapping/:id"
-              component={MappingDetailDataController}
-            />
-            <Route
-              path="/markdown/edit/:id"
-              component={MarkdownEditorDataController}
-            />
-            <Route
-              path="/markdown/:id"
-              component={MarkdownEditorDataController}
-            />
-          </Switch>
-        </Suspense>
-      </BrowserRouter>
-    );
-  }
-}
->>>>>>> feat: replace HashRouter with BrowserRouter
