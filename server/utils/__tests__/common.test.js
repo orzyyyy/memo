a91090d4e19d923c3982adb3bf6375c1454033b1
orzyyyy <zy410419243@gmail.com>
2019-08-18 20:02:11 +0800

    test: update cases for utils.common (#174)
    * test: update cases for utils.common
    
    * fix: update time
    
    * fix: mock date

---
+++ server/utils/__tests__/common.test.js
@@ -0,0 +1,48 @@
+import {
+  joinWithRootPath,
+  getTimeStamp,
+  getDateStamp,
+  readJsonFile,
+  writeIntoJsonFile,
+} from '../common';
+import path from 'path';
+import fs from 'fs-extra';
+import MockDate from 'mockdate';
+
+describe('util-common', () => {
+  it('joinWithRootPath', () => {
+    const cwd = process.cwd();
+    expect(joinWithRootPath('test')).toBe(cwd + '/test');
+    expect(joinWithRootPath('/test')).toBe(cwd + '/test');
+    expect(joinWithRootPath('./test')).toBe(cwd + '/test');
+    expect(joinWithRootPath(['test'])).toBe(cwd + '/test');
+    expect(joinWithRootPath(['test', 'test2'])).toBe(cwd + '/test/test2');
+  });
+
+  it('getTimeStamp and getDateStamp', () => {
+    MockDate.set(new Date('2019-04-09T00:00:00'));
+    expect(getTimeStamp()).toBe('20190409000000');
+    expect(getDateStamp()).toBe('20190409');
+    MockDate.reset();
+  });
+
+  it('readJsonFile', () => {
+    const configFile = readJsonFile(joinWithRootPath('server/tsconfig.json'));
+    expect(configFile.compilerOptions.outDir).toBe('../bin');
+    expect(readJsonFile).toThrowError();
+    expect(() =>
+      readJsonFile(path.join(__dirname, './empty.json')),
+    ).toThrowError();
+  });
+
+  it('writeIntoJsonFile', () => {
+    const originFunc = fs.outputJson;
+    const catcher = jest.fn();
+    fs.outputJSON = jest.fn();
+    fs.outputJSON.mockReturnValue({ catch: catcher });
+    const outputJSON = jest.spyOn(fs, 'outputJSON');
+    writeIntoJsonFile('server/utils/__tests__/test-write.json', { test: 1 });
+    expect(outputJSON).toHaveBeenCalled();
+    fs.outputJSON = originFunc;
+  });
+});
