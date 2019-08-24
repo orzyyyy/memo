033d4a4e76b7fd0768be0c3151362610dc401e09
orzyyyy <zy410419243@gmail.com>
2019-08-25 07:21:43 +0800

    refactor: ExhentaiListDataController to pure function (#188)

--- src/controller/ExHentaiListDataController.tsx
+++ src/controller/ExHentaiListDataController.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ExHentaiList, { DownloadProps } from '../pages/ExHentaiList';
 import { ExHentaiInfoItem } from '../../server/controller/ExhentaiController';
 import { Empty, message } from 'antd';
@@ -7,31 +7,30 @@ export interface ExHentaiListDataControllerProps {
   dataSource: ExHentaiInfoItem[];
 }
 
-export default class ExHentaiListDataController extends Component<
-  ExHentaiListDataControllerProps
-> {
-  handleDownload = async (item: DownloadProps) => {
-    fetch('exhentai/download', {
-      body: JSON.stringify(item),
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(response => response.text())
-      .then(result => result === 'true' && message.success('保存完成'));
-  };
+const handleDownload = async (item: DownloadProps) => {
+  fetch('exhentai/download', {
+    body: JSON.stringify(item),
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  })
+    .then(response => response.text())
+    .then(result => result === 'true' && message.success('保存完成'));
+};
 
-  render() {
-    const { dataSource } = this.props;
-    const notify = <Empty description={'该页面仅在本地可用'} />;
-    return (
-      <>
-        {!dataSource.length && notify}
-        <ExHentaiList
-          dataSource={dataSource}
-          onDownload={this.handleDownload}
-          wrapperHeight={document.body.clientHeight - 48 - 90}
-        />
-      </>
-    );
-  }
-}
+const ExHentaiListDataController = ({
+  dataSource,
+}: ExHentaiListDataControllerProps) => {
+  const notify = <Empty description={'该页面仅在本地可用'} />;
+  return (
+    <>
+      {!dataSource.length && notify}
+      <ExHentaiList
+        dataSource={dataSource}
+        onDownload={handleDownload}
+        wrapperHeight={document.body.clientHeight - 48 - 90}
+      />
+    </>
+  );
+};
+
+export default ExHentaiListDataController;
