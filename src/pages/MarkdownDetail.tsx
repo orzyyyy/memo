459d31074474bdfdfff0222e67efc0f2c767305f
只捱宅 <zy410419243@gmail.com>
2019-09-10 07:58:03 +0800

    feat: add github-markdown-css (#204)

--- src/pages/MarkdownDetail.tsx
+++ src/pages/MarkdownDetail.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import Markdown from 'react-markdown/with-html';
+import marked from 'marked';
 import './css/MarkdownDetail.css';
+import 'github-markdown-css/github-markdown.css';
 
 export interface MarkdownDetailProps {
   dataSource: string;
 }
 
 export default (props: MarkdownDetailProps) => (
-  <div className="MarkdownDetail">
+  <div className="MarkdownDetail markdown-body">
     <div className="wrapper">
-      <div className="content">
-        <Markdown source={props.dataSource} escapeHtml={false} />
-      </div>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: marked(props.dataSource || '') }}
+      ></div>
     </div>
   </div>
 );
