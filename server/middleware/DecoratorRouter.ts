e9f0ce60ad995fb70b44359ca845b1f598c2462d
orzyyyy <zy410419243@gmail.com>
2019-09-21 15:35:42 +0800

    refactor: init router as a middleware

---
+++ server/middleware/DecoratorRouter.ts
@@ -0,0 +1,27 @@
+import { Context } from 'koa';
+import fs from 'fs-extra';
+import path from 'path';
+
+const DecoratorRouter = (
+  controllerRootPath: string,
+  callback?: (url: string) => boolean,
+) => async ({ app }: Context, next: any) => {
+  fs.readdirSync(controllerRootPath)
+    .filter((filePath: string) => path.extname(filePath) === '.js')
+    .map((controllerPath: string) => {
+      const controller = path.join(controllerRootPath, controllerPath);
+      let flag = true;
+
+      if (callback) {
+        flag = callback(controller);
+      }
+
+      if (flag) {
+        app.use(require(controller).default.routes());
+      }
+    });
+
+  await next();
+};
+
+export default DecoratorRouter;
