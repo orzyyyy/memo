2859b201d83f54cd8caaba30c14fcfef12878ce8
orzyyyy <zy410419243@gmail.com>
2019-09-23 11:12:17 +0800

    chore: remove debug hook

--- src/hooks/useFetchDocumentData.ts
+++ src/hooks/useFetchDocumentData.ts
@@ -8,6 +8,14 @@ export function useFetchDocumentData(
   onSaveCallback?: (data: DataSource) => void,
 ) {
   const ext = type === 'mapping' ? 'json' : 'md';
+  // If you want to change this,
+  // check the base url in your local, netlify and gh-pages
+  // This took me a long long time to debug this
+  //
+  // Base url in the index.html is `/`, which means
+  // if you don't want get 404 in gh-pages, you must
+  // fetch data as a related path, like `./xxx/yyy`,
+  // not `/xxx/yyy`
   const url = `./${type}/${id}/${id}.${ext}`;
   const [data, setData] = useState();
   useBindKeyDown(data, onSaveCallback);
