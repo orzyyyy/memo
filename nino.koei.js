5ba4852722c9e335c101202893abdaad3b279bf5
orzyyyy <zy410419243@gmail.com>
2019-09-23 08:31:32 +0800

    refactor: pull out plugins

--- nino.koei.js
+++ nino.koei.js
@@ -7,24 +7,28 @@ const PostCompile = require('post-compile-webpack-plugin');
 const IO = require('socket.io-client');
 const fs = require('fs-extra');
 
-const assetsFiles = fs.readdirSync(path.join(__dirname, 'src/assets'));
-const mappingFile = path.join(__dirname, 'src/assets/mapping.json');
-const targetFiles = fs
-  .readJsonSync(mappingFile)
-  .filter(item => item.visible !== false && !assetsFiles.includes(item.id));
+const initMappingFiles = () => {
+  const assetsFiles = fs.readdirSync(path.join(__dirname, 'src/assets'));
+  const mappingFile = path.join(__dirname, 'src/assets/mapping.json');
+  const targetFiles = fs
+    .readJsonSync(mappingFile)
+    .filter(item => item.visible !== false && !assetsFiles.includes(item.id));
+  return targetFiles;
+};
 
-const copyPluginProps = targetFiles.map(({ id, category }) => {
-  const src = `src/assets/${category}/${id}.${
-    category === 'mapping' ? 'json' : 'md'
-  }`;
-  const dist = `dist/assets/${category}/${id}.${
-    category === 'mapping' ? 'json' : 'md'
-  }`;
-  return {
-    from: path.join(__dirname, src),
-    to: path.join(__dirname, dist),
-  };
-});
+const getCopyPluginProps = mappings =>
+  mappings.map(({ id, category }) => {
+    const src = `src/assets/${category}/${id}.${
+      category === 'mapping' ? 'json' : 'md'
+    }`;
+    const dist = `dist/assets/${category}/${id}.${
+      category === 'mapping' ? 'json' : 'md'
+    }`;
+    return {
+      from: path.join(__dirname, src),
+      to: path.join(__dirname, dist),
+    };
+  });
 
 const commonHtmlWebpackProps = {
   template: './src/index.html',
@@ -32,12 +36,12 @@ const commonHtmlWebpackProps = {
   socket:
     process.env.BUILD_ENV !== 'prod'
       ? `
-      <script src="https://cdn.jsdelivr.net/npm/socket.io-client@2/dist/socket.io.js"></script>
-      <script>
-        io('http://localhost:9099').on('refresh', function() {
-          location.reload();
-        });
-      </script>`
+        <script src="https://cdn.jsdelivr.net/npm/socket.io-client@2/dist/socket.io.js"></script>
+        <script>
+          io('http://localhost:9099').on('refresh', function() {
+            location.reload();
+          });
+        </script>`
       : '',
   inject: 'body',
   minify: {
@@ -48,51 +52,57 @@ const commonHtmlWebpackProps = {
   },
 };
 
-const htmlPluginDetailProps = targetFiles.map(({ id, category }) => {
-  const chunks = [];
+const getHtmlPluginProps = mappings => {
+  const mainPageProps = [
+    new HtmlWebpackPlugin({
+      ...commonHtmlWebpackProps,
+      filename: 'index.html',
+      chunks: ['main-page'],
+    }),
+  ];
+  const detailPageProps = [];
+  const editorPageProps = [];
 
-  switch (category) {
-    case 'mapping':
-      chunks.push('mapping-detail');
-      break;
+  mappings.map(({ id, category }) => {
+    switch (category) {
+      case 'mapping':
+        detailPageProps.push(
+          new HtmlWebpackPlugin({
+            ...commonHtmlWebpackProps,
+            filename: `${category}/${id}/index.html`,
+            chunks: ['mapping-detail'],
+          }),
+        );
+        break;
 
-    case 'markdown':
-      chunks.push('markdown-detail');
-      chunks.push('markdown-editor');
-      break;
+      case 'markdown':
+        detailPageProps.push(
+          new HtmlWebpackPlugin({
+            ...commonHtmlWebpackProps,
+            filename: `${category}/${id}/index.html`,
+            chunks: ['markdown-detail'],
+          }),
+        );
+        editorPageProps.push(
+          new HtmlWebpackPlugin({
+            ...commonHtmlWebpackProps,
+            filename: `markdown-editor/${id}/index.html`,
+            chunks: ['markdown-editor'],
+          }),
+        );
+        break;
 
-    default:
-      break;
-  }
-
-  return new HtmlWebpackPlugin({
-    ...commonHtmlWebpackProps,
-    filename: `${category}/${id}/index.html`,
-    chunks,
+      default:
+        break;
+    }
   });
-});
 
-const htmlPluginEditorProps = targetFiles.map(({ id, category }) => {
-  const chunks = [];
+  return [...mainPageProps, ...detailPageProps, ...editorPageProps];
+};
 
-  switch (category) {
-    case 'mapping':
-      break;
-
-    case 'markdown':
-      chunks.push('markdown-editor');
-      break;
-
-    default:
-      break;
-  }
-
-  return new HtmlWebpackPlugin({
-    ...commonHtmlWebpackProps,
-    filename: `markdown-editor/${id}/index.html`,
-    chunks,
-  });
-});
+const readableMappingFiles = initMappingFiles();
+const copyPluginProps = getCopyPluginProps(readableMappingFiles);
+const htmlPluginProps = getHtmlPluginProps(readableMappingFiles);
 
 const exhentaiFiles = [];
 if (process.env.BUILD_ENV !== 'prod') {
@@ -103,13 +113,7 @@ if (process.env.BUILD_ENV !== 'prod') {
 }
 
 const plugins = [
-  ...htmlPluginDetailProps,
-  ...htmlPluginEditorProps,
-  new HtmlWebpackPlugin({
-    ...commonHtmlWebpackProps,
-    filename: 'index.html',
-    chunks: ['main-page'],
-  }),
+  ...htmlPluginProps,
   new CopyWebpackPlugin([
     {
       from: path.join(__dirname, 'src/assets'),
