fdb71d5f8051c73f3f6a20448752949d070cf5be
orzyyyy <zy410419243@gmail.com>
2019-10-16 09:11:12 +0800

    fix: remove redundant Hotjar scripts

--- nino.koei.js
+++ nino.koei.js
@@ -99,6 +99,24 @@ const commonHtmlWebpackProps = {
         </script>`
       : '',
   inject: 'body',
+  hotjar: `
+  <script>
+    (function(h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function() {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: ${
+        process.env.PUBLISH_TO === 'github' ? 1529640 : 1529646
+      }, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');</script>
+    `,
   minify: {
     minifyJS: true,
     minifyCSS: true,
