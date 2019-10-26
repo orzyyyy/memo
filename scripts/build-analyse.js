3ee8c18b746c47315ca5bf2bafc2ea719d1faf7f
orzyyyy <zy410419243@gmail.com>
2019-10-26 09:57:27 +0800

    refactor: with analyse

--- scripts/build-analyse.js
+++ scripts/build-analyse.js
@@ -1,47 +1,6 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-const PostCompile = require('post-compile-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-const IO = require('socket.io-client');
-const { getCopyPluginProps, getHtmlPluginProps, compressJSON, getEntry } = require('./utils');
+const devConfig = require('./build-dev');
 
-const commonHtmlWebpackProps = {
-  environment: '<script>window.__isLocal = 1;</script>',
-  base: '<base href="/" />',
-  socket: `
-    <script src="https://cdn.jsdelivr.net/npm/socket.io-client@2/dist/socket.io.js"></script>
-    <script>
-      io('http://localhost:9099').on('refresh', function() {
-        location.reload();
-      });
-    </script>
-  `,
-  inject: 'body',
-  hotjar: '',
-  googleAnalytics: '',
-};
+devConfig.plugins.push(new BundleAnalyzerPlugin());
 
-// Don't merge these plugins into utils, for customed plugins
-const copyPluginProps = getCopyPluginProps(true);
-const htmlPluginProps = getHtmlPluginProps(commonHtmlWebpackProps);
-
-const plugins = [
-  ...htmlPluginProps,
-  new CopyWebpackPlugin(copyPluginProps),
-  new MomentLocalesPlugin({
-    localesToKeep: ['zh-cn'],
-  }),
-  new BundleAnalyzerPlugin(),
-  new PostCompile(() => {
-    // live reload
-    const socket = IO('http://localhost:9099');
-    socket.emit('refresh');
-
-    compressJSON();
-  }),
-];
-
-module.exports = {
-  entry: getEntry(),
-  plugins,
-};
+module.exports = devConfig;
