09ead4ecd95d14cd4536e1b931cd7da512f4ae0c
只捱宅 <zy410419243@gmail.com>
2019-10-29 15:23:52 +0800

    feat: add stock-shipment interfaces (#287)
    * feat: add stock-shipment interfaces
    
    * fix: parse error without ; at the end

--- server/utils/__tests__/sql.test.ts
+++ server/utils/__tests__/sql.test.ts
@@ -2,12 +2,9 @@ import { replacePlaceholderWithParams } from '../sql';
 
 describe('sql', () => {
   it('replacePlaceholderWithParams', () => {
-    expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = 1;',
-        {},
-      ),
-    ).toBe('SELECT * FROM dictionary where value = 1;');
+    expect(replacePlaceholderWithParams('SELECT * FROM dictionary where value = 1;', {})).toBe(
+      'SELECT * FROM dictionary where value = 1;',
+    );
 
     expect(
       replacePlaceholderWithParams(
@@ -18,46 +15,39 @@ describe('sql', () => {
     ).toBe(`SELECT * FROM dictionary where
         value = 1;`);
 
-    expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value;',
-        { value: 33 },
-      ),
-    ).toBe('SELECT * FROM dictionary where value = 33;');
+    expect(replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value;', { value: 33 })).toBe(
+      'SELECT * FROM dictionary where value = 33;',
+    );
+
+    expect(replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value;', { value: 'test' })).toBe(
+      // eslint-disable-next-line quotes
+      "SELECT * FROM dictionary where value = 'test';",
+    );
 
     expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value;',
-        { value: 'test' },
-      ),
-    ).toBe('SELECT * FROM dictionary where value = \'test\';');
-
-    expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value and id = @id;',
-        { value: 33 },
-      ),
+      replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value and id = @id;', { value: 33 }),
     ).toBe('SELECT * FROM dictionary where value = 33 and id = @id;');
 
     expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value and id = @id;',
-        { value: 33, id: '' },
-      ),
-    ).toBe('SELECT * FROM dictionary where value = 33 and id = \'\';');
+      replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value and id = @id;', {
+        value: 33,
+        id: '',
+      }),
+      // eslint-disable-next-line quotes
+    ).toBe("SELECT * FROM dictionary where value = 33 and id = '';");
 
     expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value and id = @id;',
-        { value: 33, id: null },
-      ),
+      replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value and id = @id;', {
+        value: 33,
+        id: null,
+      }),
     ).toBe('SELECT * FROM dictionary where value = 33 and id = null;');
 
     expect(
-      replacePlaceholderWithParams(
-        'SELECT * FROM dictionary where value = @value and id = @id;',
-        { value: 33, id: undefined },
-      ),
+      replacePlaceholderWithParams('SELECT * FROM dictionary where value = @value and id = @id;', {
+        value: 33,
+        id: undefined,
+      }),
     ).toBe('SELECT * FROM dictionary where value = 33 and id = @id;');
 
     expect(
@@ -68,5 +58,25 @@ describe('sql', () => {
       ),
     ).toBe(`SELECT * FROM dictionary where value = 33
         and id = '';`);
+
+    expect(
+      replacePlaceholderWithParams(
+        `insert into stock-shipment (type, length, width, height, weight, patch, material_cost, freight, material_type)
+          values (@type, @length, @width, @height, @weight, @patch, @materialCost, @freight, @materialType)`,
+        {
+          type: 0,
+          length: 100,
+          width: 101,
+          height: 102,
+          weight: 103,
+          patch: '201910291159',
+          materialCost: 104.1,
+          freight: 105.1,
+          materialType: 1,
+        },
+      ),
+    )
+      .toBe(`insert into stock-shipment (type, length, width, height, weight, patch, material_cost, freight, material_type)
+          values (0, 100, 101, 102, 103, '201910291159', 104.1, 105.1, 1)`);
   });
 });
