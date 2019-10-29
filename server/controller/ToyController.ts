8440e776fee4e26eaa4777dce5a9221153f7f3b7
orzyyyy <zy410419243@gmail.com>
2019-10-29 10:44:23 +0800

    feat: add stock-shipment interfaces

--- server/controller/ToyController.ts
+++ server/controller/ToyController.ts
@@ -2,27 +2,23 @@ import { Controller, Request } from '../utils/decorator';
 import ToyService from '../service/ToyService';
 import { Context } from 'koa';
 
-const getData = async (key: string, query: any) => {
-  const service = new ToyService();
-  const { result } = await service.getDataBySqlKey(key, query);
-  return result;
-};
-
-const common = async (ctx: Context) => {
-  const { key } = ctx.params;
-  const result = await getData(key, ctx.query);
-  return result;
-};
-
 @Controller('/toy')
 export default class ToyController {
+  constructor(private service: ToyService) {
+    this.service = new ToyService();
+  }
+
   @Request({ url: '/get/:key', method: 'get' })
   async getDataByGet(ctx: Context) {
-    return common(ctx);
+    const { key } = ctx.params;
+    const { result } = await this.service.getDataBySqlKey(key, ctx.query);
+    return result;
   }
 
   @Request({ url: '/post/:key', method: 'post' })
   async getDataByPost(ctx: Context) {
-    return common(ctx);
+    const { key } = ctx.params;
+    const { result } = await this.service.getDataBySqlKey(key, ctx.query);
+    return result;
   }
 }
