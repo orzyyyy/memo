e81f94656b0166598b4725425c0de133c70fa6bf
orzyyyy <zy410419243@gmail.com>
2019-10-30 07:56:43 +0800

    chore: init material-ui

--- src/controller/StockAndShipmentDataController.tsx
+++ src/controller/StockAndShipmentDataController.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import StockAndShipment from '../pages/StockAndShipment';
 
 const StockAndShipmentDataController = () => {
-  return <div>stock-shipment</div>;
+  return <StockAndShipment />;
 };
 
 export default StockAndShipmentDataController;
---
+++ src/pages/StockAndShipment.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+
+const StockAndShipment = () => (
+  <Button variant="contained" color="primary">
+    Hello World
+  </Button>
+);
+
+export default StockAndShipment;
