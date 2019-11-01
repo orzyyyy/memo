e5089c8795bc0d1adc676b2800f3082f920fcd6c
orzyyyy <zy410419243@gmail.com>
2019-11-01 16:26:47 +0800

    test: update cases

--- src/pages/__tests__/StockAndShipment.test.tsx
+++ src/pages/__tests__/StockAndShipment.test.tsx
@@ -37,6 +37,14 @@ describe('StockAndShipment', () => {
     calcuteType: [{ text: 'typeText', value: 'typeValue' }],
   };
 
+  const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+  const warnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+
+  afterAll(() => {
+    errorSpy.mockRestore();
+    warnSpy.mockRestore();
+  });
+
   it('render correctly', () => {
     const onChange = jest.fn();
     const onSubmit = jest.fn();
@@ -51,5 +59,170 @@ describe('StockAndShipment', () => {
       />,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('AppBar should work', () => {
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+    const onSpecificationInputBlur = jest.fn();
+    const wrapper: any = mount(
+      <StockAndShipment
+        onChange={onChange}
+        onSubmit={onSubmit}
+        formData={formData}
+        formOptions={formOptions}
+        onSpecificationInputBlur={onSpecificationInputBlur}
+      />,
+    );
+    expect(wrapper.find('h6').text()).toBe('入库');
+    wrapper
+      .find('ForwardRef(AppBar)')
+      .find('button')
+      .props()
+      .onClick({ text: '', value: '' }, 'type');
+    expect(onChange).toHaveBeenCalledWith({ text: '', value: '' }, 'type');
+    wrapper.setProps({ formData: Object.assign({}, formData, { type: 1 }) });
+    expect(wrapper.find('h6').text()).toBe('出库');
+  });
+
+  it('Select of calcuteType', () => {
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+    const onSpecificationInputBlur = jest.fn();
+    const wrapper: any = mount(
+      <StockAndShipment
+        onChange={onChange}
+        onSubmit={onSubmit}
+        formData={formData}
+        formOptions={formOptions}
+        onSpecificationInputBlur={onSpecificationInputBlur}
+      />,
+    );
+    expect(wrapper.find('ForwardRef(Select)').props().value).toBe(0);
+    wrapper
+      .find('ForwardRef(Select)')
+      .props()
+      .onChange(null, { props: { children: 'test1', value: 1 } });
+    expect(onChange).toHaveBeenCalledWith(
+      {
+        text: 'test1',
+        value: 1,
+      },
+      'select',
+    );
+    wrapper.setProps({ formData: Object.assign({}, formData, { calcuteType: 1 }) });
+    expect(wrapper.find('ForwardRef(Select)').props().value).toBe(1);
+  });
+
+  it('renderSpecification', () => {
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+    const onSpecificationInputBlur = jest.fn();
+    const wrapper: any = mount(
+      <StockAndShipment
+        onChange={onChange}
+        onSubmit={onSubmit}
+        formData={formData}
+        formOptions={formOptions}
+        onSpecificationInputBlur={onSpecificationInputBlur}
+      />,
+    );
+    expect(
+      wrapper
+        .find('label')
+        .at(1)
+        .text(),
+    ).toBe('长 *');
+    expect(
+      wrapper
+        .find('label')
+        .at(2)
+        .text(),
+    ).toBe('高 *');
+    expect(wrapper.find('label')).toHaveLength(9);
+
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(1)
+      .props()
+      .onChange({ target: { value: 1000 }, key: 'length' });
+    expect(onChange).toHaveBeenCalledWith({ text: 1000, value: 1000 }, 'input', 'length');
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(2)
+      .props()
+      .onChange({ target: { value: 999 }, key: 'height' });
+    expect(onChange).toHaveBeenCalledWith({ text: 999, value: 999 }, 'input', 'height');
+
+    wrapper.setProps({ formData: Object.assign({}, formData, { calcuteType: 1 }) });
+    expect(
+      wrapper
+        .find('label')
+        .at(1)
+        .text(),
+    ).toBe('长 *');
+    expect(
+      wrapper
+        .find('label')
+        .at(2)
+        .text(),
+    ).toBe('宽 *');
+    expect(
+      wrapper
+        .find('label')
+        .at(3)
+        .text(),
+    ).toBe('高 *');
+    expect(wrapper.find('label')).toHaveLength(10);
+
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(1)
+      .props()
+      .onChange({ target: { value: 1000 }, key: 'length' });
+    expect(onChange).toHaveBeenCalledWith({ text: 1000, value: 1000 }, 'input', 'length');
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(2)
+      .props()
+      .onChange({ target: { value: 999 }, key: 'width' });
+    expect(onChange).toHaveBeenCalledWith({ text: 999, value: 999 }, 'input', 'width');
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(3)
+      .props()
+      .onChange({ target: { value: 998 }, key: 'height' });
+    expect(onChange).toHaveBeenCalledWith({ text: 998, value: 998 }, 'input', 'height');
+
+    wrapper.setProps({ formData: Object.assign({}, formData, { calcuteType: 2 }) });
+    expect(wrapper.find('label')).toHaveLength(6);
+  });
+
+  it('inputs', () => {
+    const onChange = jest.fn();
+    const onSubmit = jest.fn();
+    const onSpecificationInputBlur = jest.fn();
+    const wrapper: any = mount(
+      <StockAndShipment
+        onChange={onChange}
+        onSubmit={onSubmit}
+        formData={formData}
+        formOptions={formOptions}
+        onSpecificationInputBlur={onSpecificationInputBlur}
+      />,
+    );
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(3)
+      .props()
+      .onChange({ target: { value: 997 }, key: 'weight' });
+    expect(onChange).toHaveBeenCalledWith({ text: 997, value: 997 }, 'input', 'weight');
+
+    wrapper
+      .find('ForwardRef(Input)')
+      .at(4)
+      .props()
+      .onChange({ target: { value: 996 }, key: 'materialCost' });
+    expect(onChange).toHaveBeenCalledWith({ text: 996, value: 996 }, 'input', 'materialCost');
   });
 });
