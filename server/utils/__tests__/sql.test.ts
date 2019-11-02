49ce44dbcd5998537c61769970fe18a6892eeb93
只捱宅 <zy410419243@gmail.com>
2019-11-02 09:05:43 +0800

    feat(sql): handle with system variables (#297)

--- server/utils/__tests__/sql.test.ts
+++ server/utils/__tests__/sql.test.ts
@@ -1,4 +1,5 @@
 import { replacePlaceholderWithParams } from '../sql';
+import MockDate from 'mockdate';
 
 describe('sql', () => {
   it('replacePlaceholderWithParams', () => {
@@ -78,5 +79,35 @@ describe('sql', () => {
     )
       .toBe(`insert into stock-shipment (type, length, width, height, weight, patch, material_cost, freight, material_type)
           values (0, 100, 101, 102, 103, '201910291159', 104.1, 105.1, 1)`);
+  });
+
+  it('handleWithSystemVariables', () => {
+    MockDate.set(new Date('2019-04-09T00:00:00'));
+    expect(
+      replacePlaceholderWithParams('SELECT *, @@dateStamp as dateStamp FROM dictionary where value = 1;', {}),
+    ).toBe('SELECT *, 20190409 as dateStamp FROM dictionary where value = 1;');
+    expect(
+      replacePlaceholderWithParams('SELECT *, @@timeStamp as dateStamp FROM dictionary where value = 1;', {}),
+    ).toBe('SELECT *, 20190409000000 as dateStamp FROM dictionary where value = 1;');
+    expect(
+      replacePlaceholderWithParams(
+        `insert into stock-shipment (type, length, width, height, weight, patch, material_cost, freight, material_type, create_time, modify_time)
+          values (@type, @length, @width, @height, @weight, @patch, @materialCost, @freight, @materialType, @@dateStamp, @@timeStamp)`,
+        {
+          type: 0,
+          length: 100,
+          width: 101,
+          height: 102,
+          weight: 103,
+          patch: '201910291159',
+          materialCost: 104.1,
+          freight: 105.1,
+          materialType: 1,
+        },
+      ),
+    )
+      .toBe(`insert into stock-shipment (type, length, width, height, weight, patch, material_cost, freight, material_type, create_time, modify_time)
+          values (0, 100, 101, 102, 103, '201910291159', 104.1, 105.1, 1, 20190409, 20190409000000)`);
+    MockDate.reset();
   });
 });
