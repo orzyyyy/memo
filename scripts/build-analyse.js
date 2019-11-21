1a73f5f286463684a7cf922473d64b8bee0476d7
orzyyyy <zy410419243@gmail.com>
2019-11-21 20:30:04 +0800

    feat: add cpuprofile-webpack-plugin for analyse

--- scripts/build-analyse.js
+++ scripts/build-analyse.js
@@ -1,6 +1,8 @@
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const CpuprofileWebpackPlugin = require('cpuprofile-webpack-plugin');
 const devConfig = require('./build-dev');
 
 devConfig.plugins.push(new BundleAnalyzerPlugin());
+devConfig.plugins.push(new CpuprofileWebpackPlugin());
 
 module.exports = devConfig;
