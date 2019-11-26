229d84903ea71e5a876181cd1519a3d03256af0a
orzyyyy <zy410419243@gmail.com>
2019-11-26 19:46:52 +0800

    feat: remove Layout

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import './css/MainPage.css';
-import { Menu, Layout } from 'antd';
+import { Menu } from 'antd';
 const { SubMenu } = Menu;
-const { Content, Footer, Sider } = Layout;
 import { SelectValue } from 'antd/lib/select';
 import MainPageHeader from './MainPageHeader';
 import { MappingProps } from '../../server/controller/DocumentController';
@@ -24,12 +23,12 @@ export interface MainPageProps {
 }
 
 const renderFooter = () => (
-  <Footer style={{ textAlign: 'center' }}>
+  <footer style={{ textAlign: 'center' }}>
     <div>
       你睡了一下午，醒的时候屋子里黑漆漆、一点声音都没有。抬头望望窗外，天还没完全黑。四处摸了摸，在枕头下找到手机。打开后屏幕亮起，干净，没有一条消息
     </div>
     <div>打开电脑，打开 github。pull request 写得很菜，连 core 都在喷你，但忽然就不孤独了</div>
-  </Footer>
+  </footer>
 );
 
 const MainPage = ({
@@ -50,7 +49,7 @@ const MainPage = ({
 
   const renderSider = () => {
     return (
-      <Sider theme="light" collapsible collapsedWidth={0} zeroWidthTriggerStyle={{ top: 'calc(100vh - 65px)' }}>
+      <aside>
         <Menu selectedKeys={[siderSelectedKey || '']} mode="inline" onClick={handleMenuClick}>
           {menuData &&
             menuData.map((item: SiderProps) => {
@@ -67,7 +66,7 @@ const MainPage = ({
               );
             })}
         </Menu>
-      </Sider>
+      </aside>
     );
   };
 
@@ -75,26 +74,23 @@ const MainPage = ({
     const headerHeight = isLocal ? 48 : 0;
     const wrapperHeight = document.body.clientHeight - 90 - headerHeight;
     return (
-      <Content style={{ marginLeft: 8 }}>
-        <div
-          style={{
-            padding: '8px 16px 8px 8px',
-            background: '#fff',
-            height: wrapperHeight,
-            overflow: 'auto',
-          }}
-          className="main-page-content-wrapper"
-        >
-          {renderContent && renderContent()}
-        </div>
-      </Content>
+      <div
+        style={{
+          padding: '8px 16px 8px 8px',
+          height: wrapperHeight,
+          marginLeft: 8,
+        }}
+        className="main-page-content-wrapper"
+      >
+        {renderContent && renderContent()}
+      </div>
     );
   };
 
   return (
-    <Layout className="MainPage">
+    <div style={{ display: 'grid', gridTemplateColumns: '16% 84%' }}>
       {renderSider()}
-      <Layout>
+      <div style={{ display: 'grid', gridTemplateRows: '5% 90% 10%' }}>
         {isLocal && (
           <MainPageHeader
             onExhentaiDownload={onExhentaiDownload}
@@ -106,8 +102,8 @@ const MainPage = ({
         )}
         {renderRealContent()}
         {renderFooter()}
-      </Layout>
-    </Layout>
+      </div>
+    </div>
   );
 };
 
