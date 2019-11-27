32132ebfc4c7d058b4b6d7ebd11618af06d374cf
orzyyyy <zy410419243@gmail.com>
2019-11-27 15:12:44 +0800

    feat(MarkdownEditor): remove Col, Row, Input, Button

--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Col, Row, Input, Button } from 'antd';
 import marked from 'marked';
-const { TextArea } = Input;
 import 'github-markdown-css/github-markdown.css';
 
 export interface MarkdownEditorProps {
-  onSave: () => void;
+  onSave?: () => void;
   value: string;
   onChange: (value: string) => void;
 }
@@ -13,20 +11,11 @@ export interface MarkdownEditorState {
   textareaValue: string;
 }
 
-const MarkdownEditor = ({ value, onSave, onChange }: MarkdownEditorProps) => (
-  <Row gutter={16} style={{ padding: 15, width: '100%' }}>
-    <Col span={12}>
-      <TextArea style={{ height: '90vh', fontSize: 16 }} onChange={e => onChange(e.target.value)} value={value} />
-    </Col>
-    <Col span={12} style={{ height: '90vh', overflow: 'auto', paddingLeft: 24, fontSize: 16 }}>
-      <div className="content markdown-body" dangerouslySetInnerHTML={{ __html: marked(value || '') }} />
-    </Col>
-    <Col span={8} push={8} style={{ marginTop: 24 }}>
-      <Button block onClick={onSave}>
-        提交
-      </Button>
-    </Col>
-  </Row>
+const MarkdownEditor = ({ value, onChange }: MarkdownEditorProps) => (
+  <div style={{ display: 'grid', fontSize: 16, gridTemplateColumns: '50% 50%', padding: 24 }}>
+    <textarea style={{ marginRight: 24 }} onChange={e => onChange(e.target.value)} value={value} />
+    <div className="content markdown-body" dangerouslySetInnerHTML={{ __html: marked(value || '') }} />
+  </div>
 );
 
 export default MarkdownEditor;
