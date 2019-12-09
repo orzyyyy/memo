1aa3c416fd326068c1f9b077e11173a6cde5ebf3
orzyyyy <zy410419243@gmail.com>
2019-12-09 08:03:10 +0800

    chore(MarkdownEditor): update font-size

--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -13,7 +13,12 @@ export interface MarkdownEditorState {
 
 const MarkdownEditor = ({ value, onChange }: MarkdownEditorProps) => (
   <div style={{ display: 'grid', fontSize: 16, gridTemplateColumns: '50% 50%', padding: 24 }}>
-    <textarea style={{ marginRight: 24 }} onChange={e => onChange(e.target.value)} value={value} />
+    <textarea
+      style={{ marginRight: 24 }}
+      className="markdown-body"
+      onChange={e => onChange(e.target.value)}
+      value={value}
+    />
     <div className="content markdown-body" dangerouslySetInnerHTML={{ __html: marked(value || '') }} />
   </div>
 );
