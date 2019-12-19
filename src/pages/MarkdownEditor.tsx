39a457d7ad10be07776788e6994a67b08cd24af2
只捱宅 <zy410419243@gmail.com>
2019-12-19 10:11:19 +0800

    chore: init with component LineNumber (#369)
    * chore: get line number and column index
    
    * chore: init with component LineNumber
    
    * test: update snapshots
    
    * chore: tweak overflow of content

--- src/pages/MarkdownEditor.tsx
+++ src/pages/MarkdownEditor.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import marked from 'marked';
 import 'github-markdown-css/github-markdown.css';
+import './css/MarkdownEditor.css';
+import LineNumber from '../component/LineNumber';
 
 export interface MarkdownEditorProps {
   onSave?: () => void;
   value: string;
-  onChange: (value: string) => void;
+  onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 export interface MarkdownEditorState {
   textareaValue: string;
 }
 
-const MarkdownEditor = ({ value, onChange }: MarkdownEditorProps) => (
-  <div style={{ display: 'grid', fontSize: 16, gridTemplateColumns: '50% 50%', padding: 24 }}>
-    <textarea
-      style={{ marginRight: 24 }}
-      className="markdown-body"
-      onChange={e => onChange(e.target.value)}
-      value={value}
-    />
-    <div className="content markdown-body" dangerouslySetInnerHTML={{ __html: marked(value || '') }} />
-  </div>
-);
+const MarkdownEditor = ({ value, onChange }: MarkdownEditorProps) => {
+  const onKeyUp = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    const textLines = e.currentTarget.value.substr(0, e.currentTarget.selectionStart).split('\n');
+    const lineNumber = textLines.length;
+    const columnIndex = textLines[textLines.length - 1].length;
+    // eslint-disable-next-line
+    console.log(lineNumber + ', ' + columnIndex);
+  };
+
+  return (
+    <div className="markdown-editor-wrapper">
+      <LineNumber>
+        <textarea className="markdown-body" onChange={onChange} value={value} onKeyUp={onKeyUp} />
+      </LineNumber>
+      <div
+        className="content markdown-body"
+        style={{ overflow: 'auto' }}
+        dangerouslySetInnerHTML={{ __html: marked(value || '') }}
+      />
+    </div>
+  );
+};
 
 export default MarkdownEditor;
