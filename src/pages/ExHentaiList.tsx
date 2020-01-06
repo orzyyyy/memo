ee6c6942e2e9d15b98bc6da6810cea4426d56f2a
只捱宅 <zy410419243@gmail.com>
2020-01-06 13:39:21 +0800

    refactor: remove react-lazyload (#380)
    * refactor: remove react-lazyload
    
    * test: update snapshots
    
    * perf: load image by patch

--- src/pages/ExHentaiList.tsx
+++ src/pages/ExHentaiList.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ExHentaiInfoItem } from '../../server/controller/ExhentaiController';
-import LazyLoad from 'react-lazyload';
 import './css/exhentai-list.css';
 
 export interface DownloadProps {
@@ -14,26 +13,56 @@ export interface ExHentaiListProps {
   onDetail: (url: string) => void;
 }
 
-const renderImg = ({ onDownload, wrapperHeight, item, onDetail }: ExHentaiListProps & { item: ExHentaiInfoItem }) => (
-  <div
-    style={{ height: wrapperHeight / 2 }}
-    onClick={() => onDetail(item.detailUrl)}
-    onContextMenu={() => onDownload({ url: item.detailUrl })}
-  >
-    <img alt={item.name} src={item.thumbnailUrl} style={{ height: '100%' }} />
-  </div>
-);
+const patch = 15;
 
-const ExHentaiList = ({ dataSource = [], onDownload, wrapperHeight, onDetail }: ExHentaiListProps) => (
-  <ul className="exhentai-list">
-    {dataSource.map(item => (
-      <li key={item.detailUrl + '-' + item.postTime}>
-        <LazyLoad height={wrapperHeight} once scrollContainer=".main-page-content-wrapper">
-          {renderImg({ onDownload, wrapperHeight, item, onDetail })}
-        </LazyLoad>
-      </li>
-    ))}
-  </ul>
-);
+const ExHentaiList = ({ dataSource = [], onDownload, onDetail }: ExHentaiListProps) => {
+  const [realImgInfo, setRealImgInfo] = useState([] as (ExHentaiInfoItem & { height: number; width: number })[]);
+
+  useEffect(() => {
+    const getImgHeight = async (data: ExHentaiInfoItem[]) => {
+      const target = [];
+      for (const item of data) {
+        const img = new Image();
+        img.src = item.thumbnailUrl;
+        const params: { width: number; height: number } = await new Promise(resolve => {
+          img.onload = () => {
+            resolve({ width: img.width, height: img.height });
+          };
+        });
+        target.push({ ...params, ...item });
+      }
+      return target;
+    };
+
+    const updatePatchImg = async (data: ExHentaiInfoItem[], result: any[], patch: number) => {
+      const target = data.splice(0, patch);
+      result = [...result, ...(await getImgHeight(target))];
+      setRealImgInfo(result);
+      if (data.length) {
+        updatePatchImg(data, result, patch);
+      }
+    };
+
+    updatePatchImg(dataSource, [], patch);
+  }, [dataSource]);
+
+  return (
+    <ul className="exhentai-list">
+      {realImgInfo.map(item => {
+        return (
+          <li key={item.detailUrl}>
+            <img
+              alt={item.name}
+              src={item.thumbnailUrl}
+              style={{ height: item.height, width: item.width }}
+              onClick={() => onDetail(item.detailUrl)}
+              onContextMenu={() => onDownload({ url: item.detailUrl })}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default ExHentaiList;
