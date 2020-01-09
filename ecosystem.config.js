c7960efdbc3faf8dcac0c440e7607b84df3dfc8a
orzyyyy <zy410419243@gmail.com>
2020-01-09 08:02:45 +0800

    feat: add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'memo-server',
+      script: 'bin/index.js',
+      instances: 1,
+      autorestart: true,
+      watch: ['bin'],
+      max_memory_restart: '512M',
+      out_file: 'server/log/pm2-out.log',
+      error_file: 'server/log/pm2-error.log',
+    },
+  ],
+};
