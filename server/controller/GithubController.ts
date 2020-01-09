856d350c048ca1d758311c362e1d8370dc556e6d
orzyyyy <zy410419243@gmail.com>
2020-01-09 08:42:18 +0800

    fix: webhooks get -> post

--- server/controller/GithubController.ts
+++ server/controller/GithubController.ts
@@ -1,16 +1,11 @@
 import { Controller, Request } from '../utils/decorator';
-import { Context } from 'koa';
 import { runCmd } from 'nino-cli/dist/utils/common';
 import { joinWithRootPath } from '../utils/common';
 
 @Controller('/github')
 export default class MainPageController {
-  @Request({ url: '/update', method: 'get' })
-  async update(ctx: Context) {
-    const { token } = ctx.query;
-    if (!token) {
-      return 'failed';
-    }
+  @Request({ url: '/update', method: 'post' })
+  async update() {
     runCmd('sh', [joinWithRootPath('scripts/update.sh')]);
     return 'success';
   }
