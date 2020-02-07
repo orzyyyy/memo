d0220378c3348b37d56503e9f588c07e168733e3
只捱宅 <zy410419243@gmail.com>
2020-02-07 13:32:55 +0800

    feat: customed row and col (#421)
    * refactor: controll row and col by constant
    
    * fix: lint
    
    * chore: set default row to 2
    
    * fix: download
    
    * feat: add input to change row and col
    
    * chore: update title
    
    * fix: page crashed when changing row or col
    
    * fix: unexpected number of image when downloading, causing by default option of broswer
    
    * fix: lint
    
    * docs: update
    
    * fix: canvas not rendering the slicings

--- src/pages/SlicingImage.tsx
+++ src/pages/SlicingImage.tsx
@@ -1,33 +1,50 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, createRef, useMemo } from 'react';
 import './css/slicing-image.css';
 import Upload from '../component/Upload';
 import { UploadFile } from '../component/Upload/Uploader';
 import Button from '../component/Button';
+import Input from '../component/Input';
 
 export interface BaseDocumentProps {
   innerHTML: string;
   className?: string;
 }
+export type ColProps = { [key: number]: React.RefObject<HTMLCanvasElement> };
+
+// there is a bug
+// at the beginning, the rendering data of canvas is like
+// [
+//   [col, col, col, ...] => this is row1
+//   [col, col, col, ...] => this is row2
+// ]
+// react can not find refs of these canvas elements
+// but it works when the data is like
+// [
+//   { 0: col, 1: col, 2: col } => this is row1
+//   { 0: col, 1: col, 2: col } => this is row2
+// ]
+// this is the meaning of `ColProps`
+const generateCanvasRefArr = (rowNum: number, colNum: number) => {
+  const result: ColProps[] = [];
+  for (let i = 0; i < rowNum; i++) {
+    const col: ColProps = {};
+    for (let j = 0; j < colNum; j++) {
+      col[j] = createRef<HTMLCanvasElement>();
+    }
+    result.push(col);
+  }
+  return result;
+};
 
 const SlicingImage = ({ innerHTML, className }: BaseDocumentProps) => {
-  const canvasRow = [...Array(7).keys()];
-  const canvasSet1: any = {};
-  const canvasSet2: any = {};
-  const ctxSet1: any = {};
-  const ctxSet2: any = {};
-  for (let i of canvasRow) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    canvasSet1[i] = useRef<HTMLCanvasElement>(null);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    canvasSet2[i] = useRef<HTMLCanvasElement>(null);
-  }
   const [fileList, setFileList] = useState([] as UploadFile[]);
+  const [rowNum, setRowNum] = useState(2);
+  const [colNum, setColNum] = useState(7);
+  const canvasArr = generateCanvasRefArr(rowNum, colNum);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const canvasRef = useMemo(() => canvasArr, [rowNum, colNum]);
 
   const img2Canvas = async (url: string, row: number, col: number) => {
-    for (let i of canvasRow) {
-      ctxSet1[i] = canvasSet1[i].current.getContext('2d');
-      ctxSet2[i] = canvasSet2[i].current.getContext('2d');
-    }
     const image = new Image();
     image.src = url;
     await new Promise(resolve => {
@@ -35,65 +52,94 @@ const SlicingImage = ({ innerHTML, className }: BaseDocumentProps) => {
         const { naturalWidth, naturalHeight } = image;
         const sliceWidth = naturalWidth / row;
         const sliceHeight = naturalHeight / col;
-        for (let i of canvasRow) {
-          ctxSet1[i].drawImage(image, sliceWidth * i, 0, sliceWidth, sliceHeight, 0, 0, 35, 35);
-          ctxSet2[i].drawImage(image, sliceWidth * i, sliceHeight, sliceWidth, sliceHeight, 0, 0, 35, 35);
+        for (let i = 0; i < canvasArr.length; i++) {
+          const item = Object.values(canvasArr[i]);
+          for (let j = 0; j < item.length; j++) {
+            const instance = item[j];
+            instance.current
+              ?.getContext('2d')
+              ?.drawImage(image, sliceWidth * j, i * sliceHeight, sliceWidth, sliceHeight, 0, 0, 35, 35);
+          }
         }
-
         resolve();
       };
     });
   };
 
-  const handleDownload = () => {
+  const handleDownload = async () => {
     const download = (set: any, i: number, row: number) => {
-      const img = set[i].current.toDataURL('image/png');
+      const img = set.current.toDataURL('image/png');
       const link = document.createElement('a');
-      link.download = `${row}-${i + 1}.png`;
+      link.download = `${row + 1}-${i + 1}.png`;
       link.href = img;
       link.click();
     };
-    for (const i of canvasRow) {
-      download(canvasSet1, i, 1);
-    }
-    // hack: if not using setTimeout, when clicking download,
-    // it only download images in the first line, such as 1-x,
-    // but not for 2-x. Maybe it's a browser issue.
-    setTimeout(() => {
-      for (const i of canvasRow) {
-        download(canvasSet2, i, 2);
+    for (let i = 0; i < canvasRef.length; i++) {
+      const item = canvasRef[i];
+      for (let j = 0; j < Object.values(item).length; j++) {
+        const jtem = Object.values(item)[j];
+        // for chrome, it seems that it can only download 10 pictures at a time.
+        // so hack for it, when download completed, sleep for 100ms.
+        await new Promise(resolve => {
+          download(jtem, j, i);
+          setTimeout(() => {
+            resolve();
+          }, 100);
+        });
       }
-    }, 1000);
+    }
   };
 
-  const onChange = (changedFileList: UploadFile[]) => {
+  const onUploaderChange = (changedFileList: UploadFile[]) => {
     setFileList([...fileList, ...changedFileList]);
-    img2Canvas(changedFileList[0].url, 7, 2);
+    img2Canvas(changedFileList[0].url, colNum, rowNum);
   };
 
   const onClick = (file: UploadFile) => {
     setFileList(fileList.filter(item => item.id !== file.id));
   };
 
+  const renderCanvas = () => {
+    const result = [];
+    for (let i = 0; i < rowNum; i++) {
+      result.push(<div style={{ marginTop: i === 0 ? 0 : -6 }} key={`${i}-placeholder`} />);
+      for (let j = 0; j < colNum; j++) {
+        result.push(<canvas ref={canvasRef[i][j]} width="35" height="35" key={`${i}-${j}`} />);
+      }
+    }
+    return result;
+  };
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>, type: 'row' | 'col') => {
+    const value = e.target.value;
+    switch (type) {
+      case 'row':
+        setRowNum(parseInt(value));
+        break;
+
+      case 'col':
+        setColNum(parseInt(value));
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className={`slicing-image ${className}`}>
+      <span>行：</span>
+      <Input type="number" value={rowNum} onChange={e => onInputChange(e, 'row')} />
+      <span>列：</span>
+      <Input type="number" value={colNum} onChange={e => onInputChange(e, 'col')} />
       <Upload
         fileList={fileList}
-        onChange={onChange}
+        onChange={onUploaderChange}
         uploaderVisible={fileList.length === 0}
         onClick={onClick}
         accept="image/*"
       />
-      <div>
-        {canvasRow.map(i => (
-          <canvas ref={canvasSet1[i]} width="35" height="35" key={i} />
-        ))}
-      </div>
-      <div style={{ marginTop: -6 }}>
-        {canvasRow.map(i => (
-          <canvas ref={canvasSet2[i]} width="35" height="35" key={i} />
-        ))}
-      </div>
+      <div>{renderCanvas()}</div>
       <Button onClick={handleDownload}>下载</Button>
       <div dangerouslySetInnerHTML={{ __html: innerHTML }} />
     </div>
