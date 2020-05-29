2de17170ec0100800f5cef4b29aca783df0f7e9c
orzyyyy <zy410419243@gmail.com>
2020-05-29 14:06:33 +0800

    chore: upper

--- src/component/Footer/index.tsx
+++ src/component/Footer/index.tsx
@@ -6,7 +6,7 @@ const Footer = () => (
     <div>
       你睡了一下午，醒的时候屋子里黑漆漆、一点声音都没有。抬头望望窗外，天还没完全黑。四处摸了摸，在枕头下找到手机。打开后屏幕亮起，干净，没有一条消息
     </div>
-    <div>打开电脑，打开 github。pull request 写得很菜，连 core 都在喷你，但忽然就不孤独了</div>
+    <div>打开电脑，打开 github。Pull request 写得很菜，连 core 都在喷你，但忽然就不孤独了</div>
     <a href="http://www.beian.miit.gov.cn">IPC证：浙ICP备19050866号-1</a>
   </footer>
 );
